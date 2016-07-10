123b131fb76b1fdfed080bde391439efaa467806
Alfaholic <lon.lieberman@gmail.com>
2016-07-10 17:54:36 -0400

    My submission to Week 3
    Both the cache & solve functions offered for comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        seti <- function(inverse) i <<- inverse
+        geti <- function() i
+        list(set = set, get = get,
+             seti = seti,
+             geti = geti)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Return a matrix that is the inverse of x
+cacheSolve <- function(x, ...){
+        i <- x$geti()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        mtrx <- x$get()
+        i <- solve(mtrx, ...)
+        x$seti(i)
+        i
 }
